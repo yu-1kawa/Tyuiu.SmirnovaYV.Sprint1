66ab88128bc37183f4e2060953383e37f0f43db2
yu_1kawa <fishirisjinxluk@gmail.com>
2026-09-14 21:21:00 +0500

    new lib

---
+++ Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Lib.ver2/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Lib.ver2
+{
+    public class DataService
+    {
+        public double Calculate()
+        {
+            return 6 / 2 / 3 + 1 - 6 / 2 * 3;
+        }
+    }
+}
--- Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Test/DataServiceTest.cs
+++ Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Test/DataServiceTest.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Lib;
+﻿using Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Lib.ver2;
 namespace Tyuiu.SmirnovaYV.Sprint1.Task0.V16.Test
 {
     [TestClass]
